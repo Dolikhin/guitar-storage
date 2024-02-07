712f89e8cdbad815eb8aa72a7cb9507c1763db74
Dmitrii Dolikhin <haxall53@gmail.com>
2024-02-07 16:05:37 +0300

    Исправление опечаток

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -6,7 +6,7 @@ class Products {
   }
 
   handleSetLocationStorage(element, id) {
-    const { pushProduct, prodects } = localStorageUtil.putProducts(id);
+    const { pushProduct, products } = localStorageUtil.putProducts(id);
     
     if (pushProduct) {
       element.classList.add(this.classNameActive);
@@ -15,17 +15,19 @@ class Products {
       element.classList.remove(this.classNameActive);
       element.innerHTML = this.labelAdd;
     }
+
+    headerPage.render(products.length);
   }
 
   render() {
-    const productStore = localStorageUtil.getProducts();
+    const productsStore = localStorageUtil.getProducts();
     let htmlCatalog = '';
 
     CATALOG.forEach(({ id, name, price,img}) => {
       let activeClass = '';
       let activeText = '';
 
-      if (productStore.indexOf(id) === -1) {
+      if (productsStore.indexOf(id) === -1) {
         activeText = this.labelAdd;
       } else {
         activeClass = ' ' + this.classNameActive;
@@ -34,20 +36,21 @@ class Products {
       
 
       htmlCatalog += `
-        <li class='products-element'>
-          <span class='products-element__name'>${name}</span>
-          <img class='products-element__img' src='${img}' />
-          <span class='products-element__price'>🎸 ${price.toLocaleString()} USD</span>
-          <button class='products-element__btn${activeClass}' onclick='productsPage.handleSetLocationStorage(this, "${id}");'>
+        <li class="products-element">
+          <span class="products-element__name">${name}</span>
+          <img class="products-element__img" src="${img}" />
+          <span class="products-element__price">
+            🎸 ${price.toLocaleString()} USD
+          </span>
+          <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}');">
           ${activeText}
           </button>
-          
         </li>
       `;
     });
 
     const html = `
-      <ul class='products-container'>
+      <ul class="products-container">
         ${htmlCatalog}
       </ul>
     `;
@@ -58,4 +61,70 @@ class Products {
 
 const productsPage = new Products();
 productsPage.render();
+
+
+
+// class Products {
+//   constructor() {
+//     this.classNameActive = 'products-element__btn_active';
+//     this.labelAdd = 'Добавить в корзину';
+//     this.labelRemove = 'Удалить из корзины';
+//   }
+
+//   handleSetLocationStorage(element, id) {
+//     const { pushProduct, products } = localStorageUtil.putProducts(id);
+
+//     if (pushProduct) {
+//       element.classList.add(this.classNameActive);
+//       element.innerHTML = this.labelRemove;
+//     } else {
+//       element.classList.remove(this.classNameActive);
+//       element.innerHTML = this.labelAdd;
+//     }
+
+//     headerPage.render(products.length);
+//   }
+
+//   render() {
+//     const productsStore = localStorageUtil.getProducts();
+//     let htmlCatalog = '';
+
+//     CATALOG.forEach(({ id, name, price, img }) => {
+//       let activeClass = '';
+//       let activeText = '';
+
+//       if (productsStore.indexOf(id) === -1) {
+//         activeText = this.labelAdd;
+//       } else {
+//         activeClass = ' ' + this.classNameActive;
+//         activeText = this.labelRemove;
+//       }
+
+//       htmlCatalog += `
+//                 <li class="products-element">
+//                     <span class="products-element__name">${name}</span>
+//                     <img class="products-element__img" src="${img}" />
+//                     <span class="products-element__price">
+//                         ⚡️ ${price.toLocaleString()} USD
+//                     </span>
+//                     <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}');">
+//                         ${activeText}
+//                     </button>
+//                 </li>
+//             `;
+//     });
+
+//     const html = `
+//             <ul class="products-container">
+//                 ${htmlCatalog}
+//             </ul>
+//         `;
+
+//     ROOT_PRODUCTS.innerHTML = html;
+//   }
+// }
+
+// const productsPage = new Products();
+// productsPage.render();
+
 
